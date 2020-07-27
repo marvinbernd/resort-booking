7f42a2088397907d121b0ccc8b85423457175bea
Marvin Bernd <marvinbernd@Marvins-MacBook-Pro.local>
2020-07-27 19:48:18 +0200

    initial commit & route setup

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class SingleRoom extends Component {
+  render() {
+    return <div>hello form single room page</div>;
+  }
+}
+
+export default SingleRoom;
