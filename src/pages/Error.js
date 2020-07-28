769152fcdc389df5fd30b1451d795e45cac2be77
Marvin Bernd <marvinbernd@Marvins-MacBook-Pro.local>
2020-07-28 21:23:13 +0200

    add navbar and hero component

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
 
 const Error = () => {
-  return <div>hello from error page</div>;
+  return (
+    <Hero>
+      <Banner title="404" subtitle="Page not found">
+        <Link to="/" className="btn-primary">
+          Return home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 export default Error;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
 
 const Home = () => {
-  return <div>hello from home page</div>;
+  return (
+    <Hero>
+      <Banner title="Luxurious rooms" subtitle="Deluxe rooms starting at $299">
+        <Link to="/rooms" className="btn-primary">
+          Our rooms
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 export default Home;
